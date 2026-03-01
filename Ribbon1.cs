ad3dc8706c2da155f0a06c6da3ab77345caf5546
Christopher Rath <christopher.rath@isg-one.com>
2026-02-28 19:28:39 -0600

    Initial commit.  Code that demonstrates the VSTO addin is working.

---
+++ Ribbon1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using Office = Microsoft.Office.Core;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace Edit_Exclude_Dict
+{
+    [ComVisible(true)]
+    public class Ribbon1 : Office.IRibbonExtensibility
+    {
+        private Office.IRibbonUI ribbon;
+
+        public Ribbon1()
+        {
+        }
+
+        #region IRibbonExtensibility Members
+
+        public string GetCustomUI(string ribbonID)
+        {
+           return GetResourceText("Edit_Exclude_Dict.Ribbon1.xml");
+        }
+
+        #endregion
+
+        #region Ribbon Callbacks
+        // Create callback methods here. For more information about adding callback methods, visit https://go.microsoft.com/fwlink/?LinkID=271226
+
+        public void Ribbon_Load(Office.IRibbonUI ribbonUI)
+        {
+            this.ribbon = ribbonUI;
+        }
+
+        public void OnTextButton(Office.IRibbonControl control)
+        {
+            Word.Range currentRange = Globals.ThisAddIn.Application.Selection.Range;
+            currentRange.Text = "This text was added by the Ribbon.";
+        }        
+        #endregion
+
+        #region Helpers
+
+        private static string GetResourceText(string resourceName)
+        {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string[] resourceNames = asm.GetManifestResourceNames();
+            for (int i = 0; i < resourceNames.Length; ++i)
+            {
+                if (string.Compare(resourceName, resourceNames[i], StringComparison.OrdinalIgnoreCase) == 0)
+                {
+                    using (StreamReader resourceReader = new StreamReader(asm.GetManifestResourceStream(resourceNames[i])))
+                    {
+                        if (resourceReader != null)
+                        {
+                            return resourceReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
