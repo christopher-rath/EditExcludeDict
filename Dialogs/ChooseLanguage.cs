252b2aa82c8fe0f4f076590663205347d5b6e75d
Christopher Rath <christopher.rath@isg-one.com>
2026-03-05 14:34:49 -0600

    Save/restore .ini values in accordance with the form navigation.

--- Dialogs/ChooseLanguage.cs
+++ Dialogs/ChooseLanguage.cs
@@ -25,6 +25,8 @@ namespace Edit_Exclude_Dict
     public partial class ChooseLanguage : Form
     {
         private readonly IniFile iniFile = new IniFile(ThisAddIn.Constants.sIniFileNm);
+        private bool SelectLangGrpsSaved = true;
+        private bool RememberSelectionSaved = true;
 
         public ChooseLanguage()
         {
@@ -37,7 +39,9 @@ namespace Edit_Exclude_Dict
             // Populate the list box with language options.
             //lbLanguageLists.Items.Add("English (United States)");
             cbSelectLanguageGrps.Checked = iniFile.GetBool(ThisAddIn.Constants.sIniSectionHead, ThisAddIn.Constants.sIniIsSelectLanguageGroups, true);
+            SelectLangGrpsSaved = cbSelectLanguageGrps.Checked;
             cbRemeberSelection.Checked = iniFile.GetBool(ThisAddIn.Constants.sIniSectionHead, ThisAddIn.Constants.sIniIsRememberSelection, true);
+            RememberSelectionSaved = cbRemeberSelection.Checked;
         }
         private void btnCancel_Click(object sender, EventArgs e)
         {
@@ -55,12 +59,18 @@ namespace Edit_Exclude_Dict
                 switch (editExcludeList.ShowDialog())
                 {
                     case DialogResult.Cancel:
-                        // TODO: Need to restore original .ini values.
+                        // Restore .ini values that were retrieved when the form opened.
+                        iniFile.SetBool(ThisAddIn.Constants.sIniSectionHead, ThisAddIn.Constants.sIniIsSelectLanguageGroups, SelectLangGrpsSaved);
+                        iniFile.SetBool(ThisAddIn.Constants.sIniSectionHead, ThisAddIn.Constants.sIniIsRememberSelection, RememberSelectionSaved);
                         Close();
                         break;
                     case DialogResult.Retry:
+                        // We don't do anything other than leave the ChooseLanguage form open, so
+                        // that the user may change their selections and click [Next] again.
                         break;
                     case DialogResult.OK:
+                        // The EditExcludeList form saved the lists back out to the Exclude Dictionary
+                        // files, so we just Close this form.
                         Close();
                         break;
                     default:
